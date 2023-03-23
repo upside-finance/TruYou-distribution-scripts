945d4410a8f6922c675d3b06114acddba96987b2
Tejas Kothari <tejas23682320@gmail.com>
2023-03-23 11:19:49 +0800

    Scripts

---
+++ private_sale_distribution.js
@@ -0,0 +1,129 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const { default: algosdk } = require("algosdk");
+const ObjectsToCsv = require("objects-to-csv");
+const { load } = require("csv-load-sync");
+
+require("dotenv").config();
+
+const main = async () => {
+  const token =
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  const server = `https://${process.env.ALGO_NETWORK}-api.algonode.cloud`;
+  const client = new algosdk.Algodv2(token, server, "");
+  let params = await client.getTransactionParams().do();
+  const distributorAcc = algosdk.mnemonicToSecretKey(
+    process.env.DISTRIBUTOR_MNEMONIC
+  );
+  const decimals = parseInt(process.env.ASA_DECIMALS);
+  const asaID = Number(process.env.ASA_ID);
+  const month = parseInt(process.env.MONTH);
+
+  const calcTokenDistributionForMonth = (totalTokens, month) => {
+    let alreadyDistributed;
+    if (month == 1) {
+      alreadyDistributed = 0;
+    } else {
+      alreadyDistributed = totalTokens * 0.06 * (month - 1);
+    }
+
+    if (alreadyDistributed >= totalTokens) {
+      return 0;
+    } else {
+      const toBeDistributed = totalTokens * 0.06;
+      return Math.min(toBeDistributed, totalTokens - alreadyDistributed);
+    }
+  };
+
+  const processSentTx = async (receiver, amountSU) => {
+    const amountAU = BigInt(amountSU * 10 ** decimals);
+
+    const txn = algosdk.makeAssetTransferTxnWithSuggestedParams(
+      distributorAcc.addr,
+      receiver,
+      undefined,
+      undefined,
+      amountAU,
+      undefined,
+      asaID,
+      params
+    );
+
+    const signedTx = algosdk.signTransaction(txn, distributorAcc.sk);
+    await client.sendRawTransaction(signedTx.blob).do();
+    await algosdk.waitForConfirmation(client, signedTx.txID, 20);
+
+    return { txID: signedTx.txID, amount: amountAU };
+  };
+
+  const genOutputCSV = async (distributionArr, fileName) => {
+    const csv = new ObjectsToCsv(distributionArr);
+    await csv.toDisk(`./output/private_sale/${fileName}_month_${month}.csv`, {
+      append: true,
+    });
+  };
+
+  const successfulDistribution = [];
+  const unsuccessfulDistribution = [];
+
+  const csv = load(
+    "distribution_schedule/Private Sale Allotments and Allocations.csv"
+  );
+  for (const index in csv) {
+    const row = csv[index];
+
+    const address = row["Private Sale @ 0.002 Beginning 04/23"];
+    const totalTokens = parseFloat(row["Total token allotment"]);
+
+    if (!algosdk.isValidAddress(address)) {
+      console.log("Found Malformed Algorand address");
+      unsuccessfulDistribution.push({
+        Address: address,
+        Total_token_allotment: totalTokens,
+        Amount_to_be_distributed: null,
+        Error: "Malformed Algorand address",
+      });
+      continue;
+    }
+
+    if (isNaN(totalTokens)) {
+      console.log("Found Malformed total token amount");
+      unsuccessfulDistribution.push({
+        Address: address,
+        Total_token_allotment: totalTokens,
+        Amount_to_be_distributed: null,
+        Error: "Malformed total token amount",
+      });
+      continue;
+    }
+
+    const thisMonthDistribution = calcTokenDistributionForMonth(
+      totalTokens,
+      month
+    );
+
+    await processSentTx(address, thisMonthDistribution)
+      .then((v) => {
+        successfulDistribution.push({
+          Address: address,
+          Amount_distributed: v.amount,
+          TxID: v.txID,
+        });
+      })
+      .catch((v) =>
+        unsuccessfulDistribution.push({
+          Address: address,
+          Total_token_allotment: totalTokens,
+          Amount_to_be_distributed: thisMonthDistribution,
+          Error: v,
+        })
+      );
+
+    setTimeout(() => {}, 1000);
+  }
+
+  genOutputCSV(successfulDistribution, "successful_distribution");
+  genOutputCSV(unsuccessfulDistribution, "unsuccessful_distribution");
+};
+
+main();
